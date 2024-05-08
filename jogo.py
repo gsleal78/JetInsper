214799b5e959f47ef14b761ee80f2337520bb537
Bruno Fava <bruno.fava98@outlook.com>
2024-05-08 15:24:03 -0300

    cloquei o dev 3 no principal

--- jogo.py
+++ jogo.py
@@ -1,3 +1,132 @@
-#aqui vai ser o jogo mesmo
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+
+pygame.init()
+
+# ----- Gera tela principal
+WIDTH = 1000
+HEIGHT = 800
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+
+# ----- Inicia assets
+SHIP_WIDTH = 50
+SHIP_HEIGHT = 38
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('assets/img/tela inicial.webp').convert()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+logo = pygame.image.load('assets/img/logo.webp').convert_alpha()
+logo = pygame.transform.scale(logo, (WIDTH-200, HEIGHT-200))
+ship_img = pygame.image.load('assets/img/boneco voando normal .png').convert_alpha()
+ship_img = pygame.transform.scale(ship_img, (SHIP_WIDTH, SHIP_HEIGHT))
+bullet_img = pygame.image.load('assets/img/tiro.png').convert_alpha()
+
+# ----- Inicia estruturas de dados
+# Definindo os novos tipos
+class Ship(pygame.sprite.Sprite):
+    def __init__(self, img, all_sprites, all_bullets, bullet_img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.all_sprites = all_sprites
+        self.all_bullets = all_bullets
+        self.bullet_img = bullet_img
+
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self):
+        # A nova bala vai ser criada logo acima e no centro horizontal da nave
+        new_bullet = Bullet(self.bullet_img, self.rect.top, self.rect.centerx)
+        self.all_sprites.add(new_bullet)
+        self.all_bullets.add(new_bullet)
+
+
+# Classe Bullet que representa os tiros
+class Bullet(pygame.sprite.Sprite):
+    # Construtor da classe.
+    def __init__(self, img, bottom, centerx):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.centerx = centerx
+        self.rect.bottom = bottom
+        self.speedy = -10  # Velocidade fixa para cima
+
+    def update(self):
+        # A bala só se move no eixo y
+        self.rect.y += self.speedy
+
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.bottom < 0:
+            self.kill()
+
+game = True
+# Variável para o ajuste de velocidade
+clock = pygame.time.Clock()
+FPS = 30
+
+# Criando um grupo de meteoros
+all_sprites = pygame.sprite.Group()
+all_bullets = pygame.sprite.Group()
+# Criando o jogador
+player = Ship(ship_img, all_sprites, all_bullets, bullet_img)
+all_sprites.add(player)
+
+# ===== Loop principal =====
+while game:
+    clock.tick(FPS)
+
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx += 8
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx -= 8
+
+    # ----- Atualiza estado do jogo
+
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    window.blit(logo, (100, 100))
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
 
 
